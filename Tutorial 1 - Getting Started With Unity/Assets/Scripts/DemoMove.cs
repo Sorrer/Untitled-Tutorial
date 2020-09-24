7808e25dc59928b7a9edb02a486ed2827a43a6e9
Sorrer <sorrer@gmail.com>
2020-09-24 18:03:02 -0400

    Repo created, Includes Tutorial 1 and 2

---
+++ Tutorial 1 - Getting Started With Unity/Assets/Scripts/DemoMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoMove : MonoBehaviour {
+
+    public float MovementSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 currentPosition = this.transform.position;
+
+        if (Input.GetKey(KeyCode.A)) {
+
+            currentPosition.x -= 1 * Time.deltaTime * MovementSpeed;
+
+        }
+        if (Input.GetKey(KeyCode.D)) {
+
+            currentPosition.x += 1 * Time.deltaTime * MovementSpeed;
+
+        }
+        
+        
+        if (Input.GetKey(KeyCode.W)) {
+
+            currentPosition.y += 1 * Time.deltaTime * MovementSpeed;
+
+        }
+        if (Input.GetKey(KeyCode.S)) {
+
+            currentPosition.y -= 1 * Time.deltaTime * MovementSpeed;
+
+        }
+    
+        
+        this.transform.position = currentPosition;
+    }
+}
